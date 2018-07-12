9cc53b2c586033aa684683b4ca8be95fd176ad14
Kin Pi <kinlampi@gmail.com>
2018-07-12 09:46:47 -0400

    Updated icons and text for resources page

--- app/src/main/java/com/kidssaveocean/fatechanger/resources/ResourcesFragment.java
+++ app/src/main/java/com/kidssaveocean/fatechanger/resources/ResourcesFragment.java
@@ -34,25 +34,29 @@ public class ResourcesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_resources, container, false);
         studentResourceCard = (GeneralImageListCardView) view.findViewById(R.id.students_resource_card);
-        studentResourceCard.setListTitle("For Student");
+        studentResourceCard.setListTitle(getString(R.string.for_student));
 
-        List<GeneralImageListCardView.GeneralImageListCard> cardList = new ArrayList<>();
-        cardList.add(new GeneralImageListCardView.GeneralImageListCard(0,"Example 1", "sfsdfdsfdsfdsfds"));
-        cardList.add(new GeneralImageListCardView.GeneralImageListCard(0,"Example 2", "sfsdfdsfdsfdsfds"));
-        cardList.add(new GeneralImageListCardView.GeneralImageListCard(0,"Example 3", "sfsdfdsfdsfdsfds"));
-        cardList.add(new GeneralImageListCardView.GeneralImageListCard(0,"Example 4", "sfsdfdsfdsfdsfds"));
-        cardList.add(new GeneralImageListCardView.GeneralImageListCard(0,"Example 5", "sfsdfdsfdsfdsfds"));
-        cardList.add(new GeneralImageListCardView.GeneralImageListCard(0,"Example 6", "sfsdfdsfdsfdsfds"));
-        cardList.add(new GeneralImageListCardView.GeneralImageListCard(0,"Example 7", "sfsdfdsfdsfdsfds"));
-        cardList.add(new GeneralImageListCardView.GeneralImageListCard(0,"Example 8", "sfsdfdsfdsfdsfds"));
-        cardList.add(new GeneralImageListCardView.GeneralImageListCard(0,"Example 9", "sfsdfdsfdsfdsfds"));
+        List<GeneralImageListCardView.GeneralImageListCard> studentList = new ArrayList<>();
+        studentList.add(new GeneralImageListCardView.GeneralImageListCard(R.drawable.letter, getString(R.string.sample_letters), getString(R.string.sample_letters_description)));
+        studentList.add(new GeneralImageListCardView.GeneralImageListCard(R.drawable.contacts,getString(R.string.country_contacts), getString(R.string.country_contacts_description)));
+        studentList.add(new GeneralImageListCardView.GeneralImageListCard(0,"Resources Tile", "Description goes here."));
+        studentList.add(new GeneralImageListCardView.GeneralImageListCard(0,"Resources Tile", "Description goes here."));
+        studentList.add(new GeneralImageListCardView.GeneralImageListCard(0,"Resources Tile", "Description goes here."));
 
-        GeneralImageListCardView.GeneralImageListCardViewAdapter adapter = new GeneralImageListCardView.GeneralImageListCardViewAdapter(getContext(), cardList);
-        studentResourceCard.setAdapter(adapter);
+        GeneralImageListCardView.GeneralImageListCardViewAdapter studentAdapter = new GeneralImageListCardView.GeneralImageListCardViewAdapter(getContext(), studentList);
+        studentResourceCard.setAdapter(studentAdapter);
+
+        List<GeneralImageListCardView.GeneralImageListCard> teacherList = new ArrayList<>();
+        teacherList.add(new GeneralImageListCardView.GeneralImageListCard(R.drawable.teaching, getString(R.string.teaching_9_to_12), ""));
+        teacherList.add(new GeneralImageListCardView.GeneralImageListCard(R.drawable.teaching,getString(R.string.teaching_13_to_15), ""));
+        teacherList.add(new GeneralImageListCardView.GeneralImageListCard(R.drawable.teaching,getString(R.string.teaching_16_to_18), ""));
+        teacherList.add(new GeneralImageListCardView.GeneralImageListCard(0,"Resources Tile", "Description goes here."));
+        teacherList.add(new GeneralImageListCardView.GeneralImageListCard(0,"Resources Tile", "Description goes here."));
 
         teacherResourceCard = (GeneralImageListCardView) view.findViewById(R.id.teachers_resource_card);
-        teacherResourceCard.setListTitle("For Teacher");
-        teacherResourceCard.setAdapter(adapter);
+        GeneralImageListCardView.GeneralImageListCardViewAdapter teacherAdapter = new GeneralImageListCardView.GeneralImageListCardViewAdapter(getContext(), teacherList);
+        teacherResourceCard.setListTitle(getString(R.string.for_teacher));
+        teacherResourceCard.setAdapter(teacherAdapter);
 
 
         return view;
