23bd99ed9fc3dbe735a2ffec02d88d1ac56d9851
Kin Pi <kinlampi@gmail.com>
2019-02-03 12:00:13 -0600

    Converted Home to Kotlin

---
+++ app/src/main/java/com/kidssaveocean/fatechanger/home/HomeFragment.kt
@@ -0,0 +1,38 @@
+package com.kidssaveocean.fatechanger.home
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.kidssaveocean.fatechanger.R
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val firstFragment = childFragmentManager.findFragmentById(R.id.first_fragment) as HomeCardFragment?
+        val secondFragment = childFragmentManager.findFragmentById(R.id.second_fragment) as HomeCardFragment?
+        val thirdFragment = childFragmentManager.findFragmentById(R.id.third_fragment) as HomeCardFragment?
+
+        firstFragment?.setBackgroundImage(R.drawable.letter_writing_map)
+        firstFragment?.setTypeText(R.string.updates)
+        firstFragment?.setTitleText(R.string.letter_writing_update)
+        firstFragment?.setSubtitleTextView(R.string.see_our_progress)
+
+        secondFragment?.setBackgroundImage(R.drawable.surfer)
+        secondFragment?.setTypeText(R.string.interview)
+        secondFragment?.setTitleText(R.string.peder_hill)
+        secondFragment?.setSubtitleTextView(R.string.q_and_a_with_founder)
+
+        thirdFragment?.setBackgroundImage(R.drawable.dolphins)
+        thirdFragment?.setTypeText(R.string.updates)
+        thirdFragment?.setTitleText(R.string.high_scores)
+        thirdFragment?.setSubtitleTextView(R.string.see_where_your_country_ranks)
+
+        return view
+    }
+
+}
