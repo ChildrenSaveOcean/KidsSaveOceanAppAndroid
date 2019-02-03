6b4aebc26c6347142795afa654f06618f5df34d6
Kin Pi <kinlampi@gmail.com>
2019-02-03 13:29:03 -0600

    Fixed bug in donate button

--- app/src/main/java/com/kidssaveocean/fatechanger/donation/DonationFragment.kt
+++ app/src/main/java/com/kidssaveocean/fatechanger/donation/DonationFragment.kt
@@ -8,18 +8,21 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 import com.kidssaveocean.fatechanger.R
 
 class DonationFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-            inflater.inflate(R.layout.fragment_donation, container, false)
-
-    fun pressDonateButton(view : View) {
-        val uri = Uri.parse("https://www.paypal.com")
-        val intent = Intent(Intent.ACTION_VIEW, uri)
-        startActivity(intent)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view  = inflater.inflate(R.layout.fragment_donation, container, false)
+        val donateButton = view.findViewById<Button>(R.id.donate_button)
+        donateButton.setOnClickListener {v ->
+            val uri = Uri.parse("https://www.paypal.com")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+        return view
     }
 
 }
