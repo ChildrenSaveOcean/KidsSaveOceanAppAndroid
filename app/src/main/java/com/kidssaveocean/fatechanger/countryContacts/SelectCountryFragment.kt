919b4ea7cbed4d2228192928c24563f74415b851
Denis Gordin <gordin.dan@gmail.com>
2019-08-16 09:09:31 -0400

    add finding your country func

--- app/src/main/java/com/kidssaveocean/fatechanger/countryContacts/SelectCountryFragment.kt
+++ app/src/main/java/com/kidssaveocean/fatechanger/countryContacts/SelectCountryFragment.kt
@@ -1,39 +1,51 @@
 package com.kidssaveocean.fatechanger.countryContacts
 
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.NumberPicker
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.kidssaveocean.fatechanger.R
 import com.kidssaveocean.fatechanger.bottomNavigation.BottomNavigationActivity
 import com.kidssaveocean.fatechanger.extensions.addToNavigationStack
 import com.kidssaveocean.fatechanger.firebase.CountryModel
 import com.kidssaveocean.fatechanger.firebase.FirebaseService
 import java.util.*
-import kotlin.collections.ArrayList
 
 
 class SelectCountryFragment : Fragment(), Observer {
 
+    private val PERMISSION_ACCESS_FINE_LOCATION : Int = 111
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     private var countries : List<CountryModel> = listOf()
     private var letterAddress : String? = null
+    private var picker : NumberPicker? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
+        val bottomActivity = activity as BottomNavigationActivity
 
         if (countries.isEmpty()) {
             countries = FirebaseService.getInstance().countries
             letterAddress = countries[0]?.country_address
         }
 
-        var view = inflater.inflate(R.layout.fragment_letter, container, false)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(bottomActivity)
+
+        val view = inflater.inflate(R.layout.fragment_letter, container, false)
 
         val countryNames = countries.map { it.country_name }.toTypedArray()
-        val picker = view.findViewById(R.id.country_picker) as NumberPicker?
+        picker = view.findViewById(R.id.country_picker) as NumberPicker?
         picker?.minValue = 0
         picker?.maxValue = countries.size - 1
         picker?.displayedValues = countryNames
@@ -44,11 +56,10 @@ class SelectCountryFragment : Fragment(), Observer {
         }
         val button = view.findViewById(R.id.submit_button) as Button?
         button?.setOnClickListener {
-            var bundle = Bundle()
+            val bundle = Bundle()
             bundle.putString("address", letterAddress)
-            var fragment = CountryInfoFragment()
+            val fragment = CountryInfoFragment()
             fragment.arguments = bundle
-            val bottomActivity = activity as BottomNavigationActivity
             fragment.addToNavigationStack(
                     bottomActivity.supportFragmentManager,
                     R.id.fragment_container,
@@ -58,6 +69,22 @@ class SelectCountryFragment : Fragment(), Observer {
         return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val bottomActivity = activity as BottomNavigationActivity
+        if (ContextCompat.checkSelfPermission(bottomActivity, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+            ActivityCompat.requestPermissions(
+                    bottomActivity,
+                    arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                    PERMISSION_ACCESS_FINE_LOCATION
+            )
+        }
+        else {
+            obtainLocation()
+        }
+    }
+
     override fun update(o: Observable?, arg: Any?) {
         when (o) {
             is FirebaseService -> {
@@ -65,5 +92,50 @@ class SelectCountryFragment : Fragment(), Observer {
                 letterAddress = countries[0]?.country_address
             }
         }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSION_ACCESS_FINE_LOCATION -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    obtainLocation()
+                }
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun obtainLocation() {
+        fusedLocationClient.lastLocation
+                .addOnSuccessListener { location: Location? ->
+                    location?.let { it ->
+                        val latitude = it.latitude
+                        val longitude = it.longitude
+                        val country = findCountry(latitude, longitude)
+                        country?.let { c ->
+                            val index = countries.indexOf(c)
+                            picker?.value = index
+                        }
+                    }
+                }
+    }
+
+    private fun findCountry(latitude : Double, longitude : Double): CountryModel? {
+        var closestCoutry : CountryModel? = null
+        var minDistance = 0.0
+        val startPoint = Location("myLocation")
+        startPoint.latitude = latitude
+        startPoint.longitude = longitude
+        for (country in countries) {
+            val endPoint = Location("capitalLocation")
+            endPoint.latitude = country.latitude
+            endPoint.longitude = country.longitude
+            val distance = startPoint.distanceTo(endPoint).toDouble()
+            if (minDistance == 0.0 || distance < minDistance) {
+                minDistance = distance
+                closestCoutry = country
+            }
+        }
+        return closestCoutry
     }
 }
