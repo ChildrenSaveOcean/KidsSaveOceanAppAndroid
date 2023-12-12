58572fbf601b99a45f61fcd2b9efe446514f1603
OTSS <nikolachachev97@gmail.com>
2023-12-12 17:42:55 +0200

    Finalize the new AbstractActivity and AbstractFragment that will be the base of the application

---
+++ app/src/main/java/com/kidssaveocean/fatechanger/presentation/mvvm/vm/IViewModel.kt
@@ -0,0 +1,45 @@
+@file:Suppress("KDocUnresolvedReference")
+
+package com.kidssaveocean.fatechanger.presentation.mvvm.vm
+
+import android.os.Bundle
+
+interface IViewModel {
+
+
+    /**
+     * This method is called when view model is resumed.
+     * The view model lifecycle is bound to the views lifecycle.
+     * Note that if the view is activity
+     * this method will be called from the [Activity.onResume].
+     * If the view is fragment this method may be called
+     * from [Fragment.onResume] or [Fragment.onHiddenChanged].
+     */
+    fun onResume()
+
+    /**
+     * This method is called when view model is paused.
+     * The view model lifecycle is bound to the views lifecycle.
+     * Note that if the view is of type activity
+     * this method will be called from the [Activity.onPause].
+     * If the view is of type fragment this method may be called
+     * from [Fragment.onPause] or [Fragment.onHiddenChanged].
+     */
+    fun onPause()
+
+    /**
+     * Override this method if you want to pass arguments
+     * to the next screen that is about to open.
+     *
+     * @return the arguments that will be passed
+     */
+    fun postNavigationArgs(): Bundle?
+
+    /**
+     * Override this method to receive navigation arguments
+     * when this view model is resumed.
+     *
+     * @param args the arguments that are passed, this may be null
+     */
+    fun receiveNavigationArgs(args: Bundle?)
+}
